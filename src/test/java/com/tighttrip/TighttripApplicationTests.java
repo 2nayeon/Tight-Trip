789018545f710dbe6bde0693ea2edd5892c82820
2nayeon <leenayeon0915@gmail.com>
2026-03-23 10:33:48 +0900

    feat: 프로젝트 초기 세팅 및 보안 설정 완료

---
+++ src/test/java/com/tighttrip/TighttripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tighttrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TighttripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
